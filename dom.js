244a4c370dce2cf638f70f878c66f7c044b68f63
Fuad Jamal <jamalfuad34@gmail.com>
2024-11-26 17:38:41 +0200

    applying dom

---
+++ dom.js
@@ -0,0 +1,6 @@
+document.getElementById('11').innerHTML='wazaa';
+let a = document.getElementById("linkedin").innerHTML;
+document.getElementById("wee").innerHTML='dot'+ a
+console.log(getElementById(a))
+<button onclick="displayDate()">linkedin</button>
+document.getElementById("linkedin").onclick = displayDate;
